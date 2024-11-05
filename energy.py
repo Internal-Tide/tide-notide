44f62ee5ac1ed187bea32fda840f2b155c43d769
tianzhiwei <tianzhiwei@stu.ouc.edu.cn>
2024-11-05 14:45:02 +0800

    modified:   energy.py

--- energy.py
+++ energy.py
@@ -4,8 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 #%%
-labels = ['month','day','ek','ape','eb','temp','salinity','0.1']
-da_tide = pd.read_csv('./ocn.log_tide',skiprows=4,sep='\s+',
+labels = ['month','day','ek','ape','sst','temp','salinity','0.1']
+da_tide = pd.read_csv('./ocn_tide-03-22.log',skiprows=4,sep='\s+',
                       names=labels,
                       ) 
 da_tide = da_tide.drop(columns=['0.1'])
@@ -14,7 +14,7 @@ da_tide = da_tide.drop(rows_to_delete1)
 da_tide['time'] = pd.date_range(start='2016-01-01', periods=len(da_tide), freq='90s')
 da_tide[labels[2:-1]] = da_tide[labels[2:-1]].apply(lambda x: x.str.replace('D', 'E').astype(float))  
 
-da_notide = pd.read_csv('./ocn.log_notide.log',skiprows=4,sep='\s+',
+da_notide = pd.read_csv('./ocn_notide-03-31.log',skiprows=4,sep='\s+',
                         names=labels,
                         )
 da_notide = da_notide.drop(columns=['0.1'])
@@ -24,14 +24,25 @@ da_notide['time'] = pd.date_range(start='2016-01-01', periods=len(da_notide), fr
 da_notide[labels[2:-1]] = da_notide[labels[2:-1]].apply(lambda x: x.str.replace('D', 'E').astype(float))  
 
 # %%
-sns.relplot(data=da_notide,x='time',y="ek", kind="line")
-sns.relplot(data=da_tide,x='time',y="ek", kind="line")
-
-# %%
 sns.set_style("ticks")
 sns.set_context("poster")
 sns.set_palette("husl")
+#%%
+def plot_timeseries(da_tide,da_notide,variable):
+    fig, axs = plt.subplots(1, 1, figsize=(20, 8))
+    sns.lineplot(ax=axs,data=da_notide,x='time',y=variable,label='no tide')
+    sns.lineplot(ax=axs,data=da_tide,x='time',y=variable,label='tide')
+    axs.set_xlabel("Time")
+    axs.set_ylabel(variable)
 
+    sns.despine(fig,offset=10, trim=True)
+#%%
+plot_timeseries(da_tide,da_notide,'ek')
+plot_timeseries(da_tide,da_notide,'ape')
+plot_timeseries(da_tide,da_notide,'sst')
+plot_timeseries(da_tide,da_notide,'temp')
+plot_timeseries(da_tide,da_notide,'salinity')
+#%%
 fig, axs = plt.subplots(1, 1, figsize=(20, 8))
 
 sns.lineplot(ax=axs,data=da_notide,x='time',y="ek",label='no tide')
@@ -60,28 +71,3 @@ sns.despine(fig,offset=10, trim=True)
 #           mode="none") 
 # plt.subplots_adjust(wspace=0.3)
 # fig.savefig("/home/work/tzw/code/pics/sst_ivtt.png",bbox_inches='tight') 
-# %%
-fig, axs = plt.subplots(1, 1, figsize=(20, 8))
-
-sns.lineplot(ax=axs,data=da_notide,x='time',y="ape",label='no tide')
-sns.lineplot(ax=axs,data=da_tide,x='time',y="ape",label='tide')
-axs.set_xlabel("Time")
-axs.set_ylabel("APE(W)")
-
-# ax2 = axs.twinx()
-# sns.lineplot(ax=ax2, data=da_notide, x='time', y='another_variable', label='another variable', color='r')
-# ax2.set_ylabel("Another Variable")
-sns.despine(fig,offset=10, trim=True)
-# %%
-fig, axs = plt.subplots(1, 1, figsize=(20, 8))
-
-sns.lineplot(ax=axs,data=da_notide.loc[10000:,:],x='time',y="eb",label='no tide')
-sns.lineplot(ax=axs,data=da_tide.loc[10000:,:],x='time',y="eb",label='tide')
-axs.set_xlabel("Time")
-axs.set_ylabel("temp(°C)")
-
-# ax2 = axs.twinx()
-# sns.lineplot(ax=ax2, data=da_notide, x='time', y='another_variable', label='another variable', color='r')
-# ax2.set_ylabel("Another Variable")
-sns.despine(fig,offset=10, trim=True)
-# %%
